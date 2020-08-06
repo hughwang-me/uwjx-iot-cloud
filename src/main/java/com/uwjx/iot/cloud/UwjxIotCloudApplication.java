d4d576ff7540f0a6ecd16548600cd9af5c97d536
Hugh Wang <18501667737@163.com>
2020-08-06 14:28:22 +0800

    by bash

---
+++ src/main/java/com/uwjx/iot/cloud/UwjxIotCloudApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.iot.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwjxIotCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UwjxIotCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/iot/cloud/UwjxIotCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.iot.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UwjxIotCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
